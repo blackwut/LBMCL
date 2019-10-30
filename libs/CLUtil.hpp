4ae18c1ecd19d6c284a86a12cf42a1450f2d1000
blackwut <albertoottimo@gmail.com>
2019-10-30 16:04:34 +0100

    Force using cl.hpp header c++ file

--- libs/CLUtil.hpp
+++ libs/CLUtil.hpp
@@ -1,12 +1,7 @@
 #pragma once
 
 #define __CL_ENABLE_EXCEPTIONS
-
-#if defined(__APPLE__) || defined(__MACOSX)
 #include "cl.hpp"
-#else 
-#include <CL/cl.hpp>
-#endif
 
 #include <iostream>
 #include <iomanip>

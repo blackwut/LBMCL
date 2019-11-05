d26a7f19cd95d70bbb6586355c9197ae1f929ee1
blackwut <albertoottimo@gmail.com>
2019-11-05 10:38:33 +0100

    Updated with new variable names and new text

--- vti2txt.py
+++ vti2txt.py
@@ -18,9 +18,9 @@ parser = argparse.ArgumentParser(description=description)
 parser.add_argument('-i', '--iterations', type=int, required=True,
                     help='number of iterations of VTI collection')
 parser.add_argument('-e', '--every', type=int, required=True,
-                    help='step number of iterations')
+                    help='step number between two iterations')
 parser.add_argument('-p', '--path', type=str, required=True,
-                    help='path containing VTI files')
+                    help='path of VTI files')
 
 
 args = parser.parse_args()
@@ -29,15 +29,15 @@ every = args.every
 path = args.path
 
 fill = '0'
-width = int(numpy.log10(iterations)) + 1
+width = int(numpy.log10(iterations)) + 1 if iterations > 0 else 1
 
 for i in range(0, iterations + 1, every):
-    name = '{path}/ldc.0.{i:{f}{w}}'.format(path=path, i=i, f=fill, w=width)
-    s = pyvista.read(name + '.vti')
-    rho = s['rho']
-    v = s['v']
-    rho_dim = s.dimensions
-    v_dim = s.dimensions
+    name = '{p}/ldc.0.{i:{f}{w}}'.format(p=path, i=i, f=fill, w=width)
+    data = pyvista.read(name + '.vti')
+    rho = data['rho']
+    v = data['v']
+    rho_dim = data.dimensions
+    v_dim = data.dimensions
     v_dim[2] *= 3
     rho_reshape = numpy.reshape(rho, rho_dim)
     v_reshape = numpy.reshape(v, v_dim)

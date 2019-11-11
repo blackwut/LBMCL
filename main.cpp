b839022decaac9268ee6c5f105b7f50e60397307
blackwut <albertoottimo@gmail.com>
2019-11-11 16:04:00 +0100

    float or double type simulation at runtime

--- main.cpp
+++ main.cpp
@@ -1,9 +1,37 @@
 #include <iostream>
 
 #include "common.h"
-#include "ArgsUtil.hpp"
-#include "CLUtil.hpp"
+#include "lbm_options.hpp"
 #include "lbmcl.hpp"
+
+template <typename T>
+void performSimulation(const lbm_options & opts)
+{
+    LBMCL<T> lbmcl(opts.dim,
+                   opts.viscosity,
+                   opts.velocity,
+                   opts.iterations,
+                   opts.every,
+                   opts.vtk_path,
+                   opts.lws,
+                   opts.stride,
+                   opts.optimize,
+                   opts.dump_path,
+                   opts.dump_map,
+                   opts.dump_f);
+
+    lbmcl.setupDevice(opts.platformID, opts.deviceID, true);
+    lbmcl.initialize();
+    lbmcl.performSimulation();
+    lbmcl.waitCompletion();
+
+    std::cout << "   Total time: " << lbmcl.totalTimeMS()   << " ms"    << std::endl;
+    std::cout << " Kernels time: " << lbmcl.kernelsTimeMS() << " ms"    << std::endl;
+    std::cout << "  Total MLUPS: " << lbmcl.MLUPS()         << " MLUPS" << std::endl;
+    std::cout << "Kernels MLUPS: " << lbmcl.kernelsMLUPS()  << " MLUPS" << std::endl;
+
+    std::cerr << lbmcl.statistics(';');
+}
 
 int main(int argc, char * argv[])
 {
@@ -11,30 +39,11 @@ int main(int argc, char * argv[])
     opts.process_args(argc, argv);
     opts.print_values();
 
-    LBMCL<real_t> lbmcl(opts.dim,
-                        opts.viscosity,
-                        opts.velocity,
-                        opts.iterations,
-                        opts.every,
-                        opts.vtk_path,
-                        opts.lws,
-                        opts.stride,
-                        opts.optimize,
-                        opts.dump_path,
-                        opts.dump_map,
-                        opts.dump_f);
-
-    lbmcl.setupDevice(opts.platformID, opts.deviceID, true);
-    lbmcl.initialize();
-    lbmcl.performSimulation();
-    lbmcl.waitCompletion();
-
-    std::cout << "   Total time: " << lbmcl.totalTimeMS() << " ms" << std::endl;
-    std::cout << " Kernels time: " << lbmcl.kernelsTimeMS() << " ms" << std::endl;
-    std::cout << "  Total MLUPS: " << lbmcl.MLUPS() << " MLUPS" << std::endl;
-    std::cout << "Kernels MLUPS: " << lbmcl.kernelsMLUPS() << " MLUPS" << std::endl;
-
-    std::cerr << lbmcl.statistics(';');
+    if (opts.use_double) {
+        performSimulation<double>(opts);
+    } else {
+        performSimulation<float>(opts);
+    }
 
     return 0;
 }
